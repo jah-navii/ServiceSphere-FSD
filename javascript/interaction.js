687f938a7fa8730beacf9e87a29491e5f166a8f7
jahnavi <jahnavipatnam@gmail.com>
2025-05-07 13:36:18 +0530

    something

--- javascript/interaction.js
+++ javascript/interaction.js
@@ -75,24 +75,3 @@ document.addEventListener('DOMContentLoaded', function() {
     window.sendUpdate = sendUpdate;
   });
 
-
-  nothing added to commit but untracked files present (use "git add" to track)
-PS C:\Users\jahna\acads\sem5\fdfed\servicesphere-fsd> git add .
-PS C:\Users\jahna\acads\sem5\fdfed\servicesphere-fsd> git commit -m "bookingformjs mongo"
-[main 65373df] bookingformjs mongo
- 1 file changed, 28 insertions(+)
- create mode 100644 javascript/bookingform.js
-PS C:\Users\jahna\acads\sem5\fdfed\servicesphere-fsd> git push origin main
-Enumerating objects: 6, done.
-Counting objects: 100% (6/6), done.
-Delta compression using up to 12 threads
-Compressing objects: 100% (4/4), done.
-Writing objects: 100% (4/4), 725 bytes | 725.00 KiB/s, done.
-Total 4 (delta 2), reused 0 (delta 0), pack-reused 0
-remote: Resolving deltas: 100% (2/2), completed with 2 local objects.
-To https://github.com/jah-navii/ServiceSphere-FSD.git
-   24b1aa1..65373df  main -> main
-PS C:\Users\jahna\acads\sem5\fdfed\servicesphere-fsd>
-
-
-
