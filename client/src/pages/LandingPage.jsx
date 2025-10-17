2db7c567011066219fc82ccc41099a717453c4a9
jahnavi <jahnavipatnam@gmail.com>
2025-10-17 09:42:56 +0530

    client and server folders made

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LandingPage.css"
+
+const LandingPage = () => {
+  return (
+    <div className="landing-body">
+      <div className="logo">
+        {/* Update image path later based on how you serve assets */}
+        <img src="/pics/logo.png" alt="ServiceSphere Logo" />
+      </div>
+      <div className="tag">Your Orbit to Assistance</div>
+      <div className="buttons">
+        <Link to="/login">
+          <button className="login">Login</button>
+        </Link>
+        <Link to="/signup">
+          <button className="signup">Sign Up</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
